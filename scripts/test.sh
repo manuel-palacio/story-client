d7c52861a500fa029a2e189cb35afb45ee0ee9c0
Manuel Palacio <emanuel.palacio@gmail.com>
2013-12-27 23:02:18 +0100

    updated permissions

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,8 @@
 
 BASE_DIR=`dirname $0`
 
-echo "yeah"
+echo ""
 echo "Starting Karma Server (http://karma-runner.github.io)"
 echo "-------------------------------------------------------------------"
 
-$BASE_DIR/../node_modules/karma/bin/karma start $BASE_DIR/../config/karma.conf.js $*
+karma start $BASE_DIR/../config/karma.conf.js $*
